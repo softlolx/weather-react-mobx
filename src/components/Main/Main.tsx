8a0be8eea18fca858862dab06ceff95d98fc42e1
softlolx <softlolx@gmail.com>
2022-12-07 12:18:40 +0300

    main temp block layouts

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,14 +1,15 @@
 import styles from './Main.module.scss';
 import * as React from 'react';
+import sunnyImg from '../../images/sunny.svg';
 
 export interface IMainProps {}
 
 export function Main(props: IMainProps) {
   return (
-    <div className="main">
+    <div className={styles.main}>
       <div className={styles.main__tempContainer}>
         <span className={styles.main__temp}>+20&#8451;</span>
-        <img src="#" alt="." className={styles.main__weatherImage}></img>
+        <img src={sunnyImg} alt="." className={styles.main__weatherImage} />
       </div>
       <div className={styles.main__infoContainer}>
         <span className={styles.main__time}>15:35</span>
