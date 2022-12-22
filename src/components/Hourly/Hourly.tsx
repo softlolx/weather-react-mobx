b2342bc94a77668441f5f46230e8f32b0e2f4156
softlolx <softlolx@gmail.com>
2022-12-23 01:19:47 +0300

    basic functionality

--- src/components/Hourly/Hourly.tsx
+++ src/components/Hourly/Hourly.tsx
@@ -3,19 +3,26 @@ import * as React from 'react';
 import { observer } from 'mobx-react-lite';
 
 import { HourlyItem } from '../HourlyItem/HourlyItem';
-// import { hourly } from '../../store/future';
+import { future } from '../../store/future';
 
 let hourly = [...new Array(12)];
 
 export interface IHourlyProps {}
-console.log(hourly);
 
 export const Hourly = observer((props: IHourlyProps) => {
   return (
     <div className={styles.hourly}>
-      {hourly.map((item) => (
-        <HourlyItem />
-      ))}
+      {
+        //@ts-ignore
+        future.forecast[0]?.hour.map((item) => (
+          <HourlyItem
+            key={item.time}
+            icon={item.condition.icon}
+            temp={item.temp_c}
+            time={item.time}
+          />
+        ))
+      }
     </div>
   );
 });
--- src/components/HourlyItem/HourlyItem.tsx
+++ src/components/HourlyItem/HourlyItem.tsx
@@ -1,16 +1,19 @@
 import styles from './HourlyItem.module.scss';
 import * as React from 'react';
 import { observer } from 'mobx-react-lite';
-import image from '../../images/248.png';
 
-export interface IHourlyItemProps {}
+export interface IHourlyItemProps {
+  temp: string;
+  icon: string;
+  time: string;
+}
 
-export const HourlyItem = observer(({}: IHourlyItemProps) => {
+export const HourlyItem = observer(({ temp, icon, time }: IHourlyItemProps) => {
   return (
     <div className={styles.hourlyItem}>
-      <span className={styles.time}>12:00</span>
-      <img src={image} alt="conditions image" className={styles.img} />
-      <span className={styles.temp}>+12&#8451;</span>
+      <span className={styles.time}>{time.slice(10)}</span>
+      <img src={icon} alt="conditions image" className={styles.img} />
+      <span className={styles.temp}>{Math.floor(+temp)}&#8451;</span>
     </div>
   );
 });
